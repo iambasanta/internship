58fe23c91068823d5059748cc14f8ffc27d102be
iambasanta <sangpangbasanta@gmail.com>
2023-05-28 13:04:27 +0545

    Implement user register and login

--- quips/index.js
+++ quips/index.js
@@ -4,9 +4,11 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const { sequelize, connect } = require("./db");
+const bodyParser = require("body-parser");
 
 app.use(express.json());
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 connect();
 const userRouter = require("./routes/user");
--- quips/routes/user.js
+++ quips/routes/user.js
@@ -4,13 +4,17 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db");
 const UserSchema = require("../models/user");
+const { registerUser, loginUser } = require("../controllers/user");
 
 router.get("/register", (req, res, next) => {
   res.render("user/register");
 });
 
+router.post("/register", registerUser);
+
 router.get("/login", (req, res, next) => {
   res.render("user/login");
 });
+router.post("/login", loginUser);
 
 module.exports = router;
