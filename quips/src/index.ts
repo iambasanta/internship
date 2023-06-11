7f556c533a78f561bf1454c03fc8ecb319aa7bae
iambasanta <sangpangbasanta@gmail.com>
2023-06-11 11:40:44 +0545

    Setup typescript

---
+++ quips/src/index.ts
@@ -0,0 +1,15 @@
+import express, { Application } from "express";
+
+const PORT = 8000;
+
+const app: Application = express();
+
+app.get("/foo", (_req, res) => {
+  res.send({
+    message: "bar",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`);
+});
