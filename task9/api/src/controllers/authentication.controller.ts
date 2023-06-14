9394e8025cc68ae645fcfd9d1acb64d3f45b9e9b
iambasanta <sangpangbasanta@gmail.com>
2023-06-14 08:13:16 +0545

    Implement user login

--- task9/api/src/controllers/authentication.controller.ts
+++ task9/api/src/controllers/authentication.controller.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import User from "../models/user";
+import bcrypt from "bcrypt";
 
 export const register = async (req: Request, res: Response) => {
   try {
@@ -11,18 +12,46 @@ export const register = async (req: Request, res: Response) => {
 
     let user = await User.findOne({ where: { email } });
     if (user) {
-      return res
-        .sendStatus(400)
-        .json({ error: "Email has already been taken" });
+      return res.sendStatus(400);
     }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     user = await User.create({
       username,
       email,
-      password,
+      password: hashedPassword,
     });
 
     return res.sendStatus(201);
+  } catch (error) {
+    console.error(error);
+    return res.sendStatus(500);
+  }
+};
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.sendStatus(400);
+    }
+
+    let user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.sendStatus(400);
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.sendStatus(400);
+    }
+
+    return res.sendStatus(200);
   } catch (error) {
     console.error(error);
     return res.sendStatus(500);
--- task9/api/src/routes/authentication.router.ts
+++ task9/api/src/routes/authentication.router.ts
@@ -1,14 +1,10 @@
 import express from "express";
-import { register } from "../controllers/authentication.controller";
+import { login, register } from "../controllers/authentication.controller";
 
 const router = express.Router();
 
 router.post("/register", register);
 
-router.post("/login", (req: express.Request, res: express.Response) => {
-  res.send({
-    message: "user login",
-  });
-});
+router.post("/login", login);
 
 export default router;
