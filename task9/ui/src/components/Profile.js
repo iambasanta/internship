e0dad19bd0ea372c83124461ebd376616240aece
iambasanta <sangpangbasanta@gmail.com>
2023-06-15 18:17:51 +0545

    Fetch user profile

--- task9/ui/src/components/Profile.js
+++ task9/ui/src/components/Profile.js
@@ -1,10 +1,39 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function Profile() {
+  const [userDetails, setUserDetails] = useState("");
+
+  const client = axios.create({
+    baseURL: "http://localhost:8000/user/profile",
+  });
+
+  useEffect(() => {
+    myPrifile();
+  }, []);
+
+  const myPrifile = async () => {
+    try {
+      const auth_token = localStorage.getItem("auth_token");
+      const response = await client.get("", {
+        headers: {
+          Authorization: `Bearer ${auth_token}`,
+        },
+      });
+
+      const user = response.data.user;
+      setUserDetails(user);
+    } catch (error) {
+      console.error("Failed to fetch your profile:", error);
+    }
+  };
+
   return (
     <>
       <div className="profile">
         <h3>My profile</h3>
-        <p>Username : John Doe</p>
-        <p>Email : john@email.com</p>
+        <p>Username : {userDetails.username}</p>
+        <p>Email : {userDetails.email}</p>
       </div>
     </>
   );
