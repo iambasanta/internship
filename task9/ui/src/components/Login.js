4af21d6906e946351a3c672ee33f4bfaf3ca6f39
iambasanta <sangpangbasanta@gmail.com>
2023-06-15 18:05:34 +0545

    Implement user register and login

--- task9/ui/src/components/Login.js
+++ task9/ui/src/components/Login.js
@@ -1,4 +1,31 @@
+import axios from "axios";
+import { useState } from "react";
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const client = axios.create({
+    baseURL: "http://localhost:8000/auth/login",
+  });
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    if (email.trim() !== "" || password.trim() !== "") {
+      try {
+        const response = await client.post("", { email, password });
+        const auth_token = response.data.auth_token;
+
+        localStorage.setItem("auth_token", auth_token);
+
+        setEmail("");
+        setPassword("");
+      } catch (error) {
+        console.error("Login failed:", error);
+      }
+    }
+  };
+
   return (
     <>
       <form className="form">
@@ -7,6 +34,8 @@ export default function Login() {
             className="input-field"
             type="email"
             name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             placeholder="Email"
           />
         </div>
@@ -15,10 +44,14 @@ export default function Login() {
             className="input-field"
             type="password"
             name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             placeholder="********"
           />
         </div>
-        <button className="button">Login</button>
+        <button onClick={handleLogin} className="button">
+          Login
+        </button>
       </form>
     </>
   );
--- task9/ui/src/components/Register.js
+++ task9/ui/src/components/Register.js
@@ -1,4 +1,37 @@
+import axios from "axios";
+import { useState } from "react";
+
 export default function Register() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const client = axios.create({
+    baseURL: "http://localhost:8000/auth/register",
+  });
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    if (
+      username.trim() !== "" ||
+      email.trim() !== "" ||
+      password.trim() !== ""
+    ) {
+      try {
+        const response = await client.post("", { username, email, password });
+        const auth_token = response.data.auth_token;
+
+        localStorage.setItem("auth_token", auth_token);
+
+        setUsername("");
+        setEmail("");
+        setPassword("");
+      } catch (error) {
+        console.error("Registration failed:", error);
+      }
+    }
+  };
+
   return (
     <>
       <form className="form">
@@ -7,6 +40,8 @@ export default function Register() {
             className="input-field"
             type="text"
             name="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
             placeholder="Username"
           />
         </div>
@@ -15,6 +50,8 @@ export default function Register() {
             className="input-field"
             type="email"
             name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             placeholder="Email"
           />
         </div>
@@ -23,10 +60,14 @@ export default function Register() {
             className="input-field"
             type="password"
             name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             placeholder="********"
           />
         </div>
-        <button className="button">Register</button>
+        <button onClick={handleRegister} className="button">
+          Register
+        </button>
       </form>
     </>
   );
