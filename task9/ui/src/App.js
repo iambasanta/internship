7e203da47c8513bfdd7d38c6380abf3264e8a947
iambasanta <sangpangbasanta@gmail.com>
2023-06-15 17:31:02 +0545

    Add navigation and profile page

--- task9/ui/src/App.js
+++ task9/ui/src/App.js
@@ -1,15 +1,35 @@
 import "./App.css";
 import Register from "./components/Register";
 import Login from "./components/Login";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <>
       <div className="container">
+        <nav className="navbar">
+          <li className="nav-item">
+            <Link to={"/register"} className="nav-link">
+              Register
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/login"} className="nav-link">
+              Login
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/profile"} className="nav-link">
+              My profile
+            </Link>
+          </li>
+        </nav>
+
         <Routes>
-          <Route path="/auth/register" element={<Register />} />
-          <Route path="/auth/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </div>
     </>
---
+++ task9/ui/src/components/Profile.js
@@ -0,0 +1,11 @@
+export default function Profile() {
+  return (
+    <>
+      <div className="profile">
+        <h3>My profile</h3>
+        <p>Username : John Doe</p>
+        <p>Email : john@email.com</p>
+      </div>
+    </>
+  );
+}
