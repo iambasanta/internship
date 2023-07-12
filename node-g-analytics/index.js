de1ec2e5ad759924d78307b5908c4a2d8a357174
iambasanta <sangpangbasanta@gmail.com>
2023-07-12 12:54:35 +0545

    Make a request

--- node-g-analytics/index.js
+++ node-g-analytics/index.js
@@ -1,60 +1,33 @@
 const express = require("express");
 const app = express();
-require("dotenv").config();
+const axios = require("axios");
 
-const { google } = require("googleapis");
+const payload = {
+  client_id: "1431843420.1689140953",
+  non_personalized_ads: false,
+  events: [
+    {
+      name: "search",
+      params: {
+        search_term: "from node application",
+        session_id: 1689140953,
+        debug_mode: 1,
+      },
+    },
+  ],
+};
 
-const scopes = "https://www.googleapis.com/auth/analytics.readonly";
-
-const jwt = new google.auth.JWT(
-  process.env.CLIENT_EMAIL,
-  null,
-  process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
-  scopes
-);
-const view_id = "293213122";
-
-async function getViews() {
-  try {
-    await jwt.authorize();
-
-    const response = await google.analytics("v3").data.ga.get({
-      auth: jwt,
-      ids: "ga:" + view_id,
-      "start-date": "30daysAgo",
-      "end-date": "today",
-      metrics: "ga:pageviews",
-    });
-
-    console.log(response);
-  } catch (err) {
-    console.log(err);
-  }
-}
-async function getTopPosts() {
-  try {
-    await jwt.authorize();
-
-    const response = await google.analytics("v3").data.ga.get({
-      auth: jwt,
-      ids: "ga:" + view_id,
-      "start-date": "2019-01-01",
-      "end-date": "today",
-      dimensions: "ga:pagePath,ga:pageTitle",
-      metrics: "ga:pageviews",
-      sort: "-ga:pageviews",
-      "max-results": "10",
-      filters: "ga:medium==organic",
-    });
-
-    console.log(response);
-  } catch (err) {
-    console.log(err);
-  }
+try {
+  axios({
+    method: "post",
+    url: "https://www.google-analytics.com/mp/collect?api_secret=hMkefMctTQqNF0JhzWRqqQ&measurement_id=G-MG0E50QD47",
+    data: payload,
+  });
+} catch (error) {
+  console.error(error);
 }
 
-getViews();
-
-getTopPosts();
-
-app.listen(3000, () => console.log(`listening on port 3000`));
+app.listen(3000, () => {
+  console.log(`listening on port 3000`);
+  console.log(`http://localhost:3000`);
+});
