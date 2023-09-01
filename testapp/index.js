648e292de498fae194fb221d2b9129bdad40cda0
iambasanta <iambasantarai@gmail.com>
2023-09-01 10:54:47 +0545

    Setup openapi

--- testapp/index.js
+++ testapp/index.js
@@ -4,27 +4,55 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const fs = require("fs")
+const fs = require("fs");
+require("dotenv").config();
+const { Configuration, OpenAIApi } = require("openai");
 
-const PORT = 8000;
-
+const PORT = process.env.PORT || 8000;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+const configuration = new Configuration({
+  apiKey: process.env.OPEN_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
-app.post("/register", (req, res) => {
-    console.log("START");
-    console.log("INPUT VALUES", req.body.inputValues);
-    console.log("HEADERS", req.headers);
-    console.log("END");
-    res.sendStatus(200);
+app.get("/support", async (req, res) => {
+  try {
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        { role: "system", content: "you are a comedian" },
+        { role: "user", content: "make me laugh with daddy jokes" },
+      ],
+    });
+
+    return res.status(200).json({
+      success: true,
+      data: response.data.choices[0].message,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      error: error.response
+        ? error.response.data
+        : "There was an issue on the server",
+    });
+  }
 });
 
-app.post("/excitement-poll", (req,res) => {
+app.post("/register", (req, res) => {
+  console.log("START");
+  console.log("INPUT VALUES", req.body.inputValues);
+  console.log("HEADERS", req.headers);
+  console.log("END");
+  res.sendStatus(200);
+});
 
-    const jsonData = { excited: req.body.inputValues.excited };
+app.post("/excitement-poll", (req, res) => {
+  const jsonData = { excited: req.body.inputValues.excited };
 
-    fs.readFile("excitement-poll.json", (err, data) => {
+  fs.readFile("excitement-poll.json", (err, data) => {
     let existingData = [];
 
     if (!err) {
@@ -42,8 +70,7 @@ app.post("/excitement-poll", (req,res) => {
       res.status(200).json({ message: "Data saved successfully!" });
     });
   });
-
-})
+});
 
 app.get("/excitement-poll", (req, res) => {
   fs.readFile("excitement-poll.json", (err, data) => {
@@ -69,13 +96,19 @@ app.get("/excitement-poll", (req, res) => {
   });
 });
 
-app.post("/event/survey", (req,res) => {
-    console.log("START");
-    console.log("INPUT VALUES", req.body.inputValues);
-    console.log("END");
-})
+app.post("/event/survey", (req, res) => {
+  console.log("START");
+  console.log("INPUT VALUES", req.body.inputValues);
+  console.log("END");
+});
+
+app.post("/test", (req, res) => {
+  console.log("START");
+  console.log("INPUT VALUES", req.body.inputValues);
+  console.log("END");
+});
 
 app.listen(PORT, () => {
-    console.log(`Listening on ${PORT}`);
-    console.log(`http://localhost:${PORT}`);
+  console.log(`Listening on ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
 });
